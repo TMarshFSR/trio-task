d06bb8492c7867073562c7428689f95892991dba
Ubuntu <ubuntu@ip-172-31-8-63.eu-west-1.compute.internal>
2021-03-23 18:33:56 +0000

    adds volume and password control

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+echo "Enter mysql root password: "
+read -s password
+
 # Create network
 docker network create trio-task-network
+
+# Create a volume
+
+docker volume create trio-db-volume
 
 # Build images
 docker build -t trio-db db
@@ -10,6 +17,9 @@ docker build -t trio-flask-app flask-app
 # Run database container
 docker run -d \
     --network trio-task-network \
+    --volume trio-db-volume:/var/lib/mysql \
+    -e MYSQL_ROOT_PASSWORD=$password \
+    -e MYSQL_DATABASE=flask-db \
     --name mysql \
     trio-db
 
